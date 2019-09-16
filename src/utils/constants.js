1e16fc9b9da9f3337d16727e3b66f5dcc73f5943
Sher Ali <sheralim012@gmail.com>
2019-09-16 17:15:07 +0500

    add/remove constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,14 +1,21 @@
 const constants = {
-  FACEBOOK_SCOPE: ["manage_pages", "pages_show_list", "read_insights"],
-  FACEBOOK_CONNECTED: "connected",
-  FACEBOOK_LOGGED_OUT: "unknown",
+  APP_ID: "349831992602224",
+  FACEBOOK_SCOPES: ["manage_pages", "pages_show_list", "read_insights"],
+  FACEBOOK_STATUS_UNKNOW: "unknown",
+  CHECK_IS_CONNECTED: "CHECK_IS_CONNECTED",
   SET_IS_CONNECTED: "SET_IS_CONNECTED",
-  FACEBOOK_PERMISSION_STATUS_GRANTED: "granted",
-  FACEBOOK_PERMISSION_STATUS_DECLINED: "declined",
+  MESSAGE_TYPES: {
+    SUCCESS: "success",
+    FAILURE: "failure",
+    INFO: "info"
+  },
   APIS: {
     ROOT: "http://localhost:8000/",
     get FACEBOOK_BENCHMARK() {
       return `${this.ROOT}facebook_benchmark/`;
+    },
+    get CHECK_IS_CONNECTED() {
+      return `${this.FACEBOOK_BENCHMARK}check_is_connected/`;
     },
     get FETCH_FACEBOOK_PROFILE() {
       return `${this.FACEBOOK_BENCHMARK}fetch_facebook_profile/`;
