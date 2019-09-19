7df5087a4479583e51b3793f1688f6503534ca78
Abdul.Azeem <abdul@A002-00112.arbisoft.com>
2019-09-19 14:47:47 +0500

    crawler status update and zip download feature implemented

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,10 +1,12 @@
-const API = 'http://localhost:8000'
+const API = 'https://3442e151.ngrok.io'
 const clientURL = 'http://localhost:3000'
 const LoginURL = `${API}/accounts/login`
 const RegisterURL = `${API}/accounts/register`
 const FetchProfileURL = `${API}/instagram_benchmark/profile`
 const FetchMediaURL = `${API}/instagram_benchmark/media?page=`
 const CrawlUserURL = `${API}/instagram_benchmark/profile/crawl`
+const CrawlStatusURL = `${API}/instagram_benchmark/profile/crawl/status`
+const CrawlImagesDownloadURL = `${API}/instagram_benchmark/profile/crawl/zip`
 const InstaRedirect =
   'https://api.instagram.com/oauth/authorize/?client_id=4d8f538893ba481f88c0614865dc9310&redirect_uri=http://127.0.0.1:3000/igconnected&response_type=token'
 const FetchNewDataURL = `${API}/instagram_benchmark/profile/load`
@@ -18,5 +20,7 @@ export {
   FetchMediaURL,
   InstaRedirect,
   FetchNewDataURL,
-  CrawlUserURL
+  CrawlUserURL,
+  CrawlStatusURL,
+  CrawlImagesDownloadURL
 }
